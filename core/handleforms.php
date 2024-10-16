c3106ff673a1819c2fac1178c29c0a285c13bb6a
Martie1668 <johnmartie.salvoro.cvt@eac.edu.ph>
2024-10-17 01:06:59 +0800

    handleforms.php

--- core/handleforms.php
+++ core/handleforms.php
@@ -3,7 +3,7 @@
 require_once("dbConfig.php");
 require_once ('models.php');
 
-if (isset($_POST['editbutton'])) {
+if (isset($_POST['insertnewbtn'])) {
 	$username = trim($_POST['username']);
 	$firstname = trim($_POST['firstname']);
 	$lastname = trim($_POST['lastname']);
@@ -30,32 +30,7 @@ if (isset($_POST['editbutton'])) {
 	
 }
 
-if (isset($_POST["register"])) {
-    echo"<pre>";
-    print_r ($_POST);
-    echo"<pre>";
 
-}
-
-if (isset($_POST['insertnewemployeebtn'])) {
-    $username = $_POST['username'];
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $profession = $_POST['profession'];
-    $statusinlife = $_POST['statusinlife'];
-    $experience = $_POST['experience'];
-    $age = $_POST['age'];
-
-    $query = insertIntoform($pdo,$user_name,$firstname,$Last_name,$profession,$statusinlife,$experience,$age);
-                            
-    if($query) {
-        header('location: ../index.php');
-    }
-    else{
-        echo"query failed";
-    }
-
-}
 if (isset($_POST['editbutton'])) {
 	$userID = $_GET['userID'];
 	$username = trim($_POST['username']);
@@ -66,24 +41,26 @@ if (isset($_POST['editbutton'])) {
 	$experience = trim($_POST['experience']);
 	$age = trim($_POST['age']);
 
-	if (!empty($username) && !empty($firstname) && !empty($lastname) && !empty($profession) && !empty($statusinlife)  && !empty($experience)  && !empty($age)) {
-		
-		$query = update_employee($pdo, $userID, $user_name, $firstname, $Last_name, $profession, $statusinlife, $experience, $Age);
+	if (!empty($userID) && !empty($username) && !empty($firstname) && !empty($lastname) && !empty($profession) && !empty($statusinlife)  && !empty($experience)  && !empty($age)) {
+			$query = update_employee($pdo, $userID,$username, $firstname, $lastname, $profession, $statusinlife, $experience, $age);
 
-	if ($query) {
-		header("Location:../index.php");
+		if ($query) {
+			header("Location: ../index.php");
+		}
+
+		else {
+			echo "Insertion failed";
+		}
 	}
 
 	else {
-		echo "Update failed";
+		echo "Make sure that no fields are empty";
 	}
+	
 }
 
-else {
-	echo "Make sure that no fields are empty";
-}
 
-}
+
 if (isset($_POST['deletebtn'])) {
 
 	$query = Delete_employee($pdo, $_GET['userID']);
